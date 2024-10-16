b6e91a45f21628630df06089a37f506b2212a58a
Fabio Di Gregorio <digre01@gmail.com>
2024-10-16 12:18:19 +0200

    Creation of the method register_users in Log-in.py

--- Log-in.py
+++ Log-in.py
@@ -1,5 +1,9 @@
 import os
 import json
+import hashlib
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
 
 # Percosrso del file json contenente i dati degli utenti
 USER_DATA = 'user_data.json'
@@ -11,7 +15,39 @@ def load_users():
             return json.load(file)
     return {}
 
-# Funzione per salvare i dati degli utenti
+# Funzione per salvare i dati degli utenti (devo decidere se salvarli in un file json o due uno per i dati e uno per le password)
 def save_users(users):
     with open(USER_DATA, 'w') as file:
         json.dump(users, file)
+
+# Funzione per registrare un nuovo utente
+def register_users(username, password):
+    users = load_users()
+    if username in users:
+        print("Username già in uso")
+        return False
+    else:
+        # Hash della password
+        password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+        # Generazione della coppia di chiavi pubblica/privata
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+        private_key_bytes = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.BestAvailableEncryption(password.encode())
+        )
+        public_key = private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        # Salvataggio dei dati dell'utente
+        users[username] = {
+            'password': password_hash,
+            'private_key': private_key_bytes.decode(),
+            'public_key': public_key.decode()
+        }
+        save_users(users)
+        print(f"Registrazione completata per l'utente {username}")
+        return True
