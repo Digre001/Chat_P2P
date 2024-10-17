3850ea42bca0d6c9718eee39f4d8c49bee1183a3
Fabio Di Gregorio <digre01@gmail.com>
2024-10-17 16:17:11 +0200

    Creation of the class Client in the file Client.py

---
+++ Client.py
@@ -0,0 +1,24 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+from random import randint 
+
+class Client(DatagramProtocol):
+    def __init__(self, host, port):
+        if host == "localhost":
+            host = "127.0.0.1"
+
+        self.id = host, port
+        self.address = None 
+        print("Working in id:", self.id)
+
+    def datagramReceived(self, datagram, addr) -> None:
+        print(addr, ":", datagram)
+    
+    def send_message(self):
+        while True: 
+            self.transport.write(input(":::").encode('utf-8'), self.address)
+
+if __name__ == "__main__":
+    port = randint(1000, 5000)
+    reactor.listenUDP(port, Client("localhost", port))
+    reactor.run()
